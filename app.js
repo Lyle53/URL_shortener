897c19a2433238bb2944c7489edce46f54f534de
Lyle53 <lyle9453@gmail.com>
2023-11-29 16:39:08 +0800

    增加隨機生成短網址 並包裝完成一個專案

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const fs = require('fs')
+const shortUrl = require('./shortUrl/randomURL')
+
+
 // View:樣版引擎負責畫面呈現
 app.engine('.hbs',engine({extname:'.hbs'}))
 app.set('view engine','.hbs')
@@ -17,6 +22,54 @@ app.get('/', (req, res) =>{
 })
 app.get('/URL_Shortener',(req, res) =>{
   res.render('index')
+})
+
+let urldata = {}   //讀取json檔案 
+if (fs.existsSync('./data.json')) {
+  try {
+    const jsonData = fs.readFileSync('./data.json')
+    urldata = JSON.parse(jsonData)
+
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+app.post('/URL_Shortener', (req, res) => {
+  const originalURL = req.body.inputURL
+
+  if (!originalURL || !originalURL.toLowerCase().trim().startsWith("http")) {   //防止輸入空字串或是不是http開頭的
+    res.render('index', { error: '請輸入正確的URL' })
+
+    return
+  }
+
+
+  if (Object.keys(urldata).includes(originalURL)) {   //辨認是否有同樣的縮網址
+    useshortUrl = urldata[originalURL]
+    res.render('index', { useshortUrl })
+  }
+
+  else {
+    const useshortUrl = shortUrl() //執行 randomURL檔案內的函數
+    urldata[originalURL] = useshortUrl
+    fs.writeFileSync('./data.json', JSON.stringify(urldata)) //輸入進data.json檔案裡
+
+    res.render('index', { useshortUrl })
+  } 
+
+})
+
+app.get('/:ShortUrl', (req, res) => {
+  const url = req.params.ShortUrl
+  for (const key in urldata) {
+    if (url === urldata[key]) {
+      res.redirect(key)  //[key] 匹配到 url 這時可以直接使用key來代表相對應的鍵
+      return
+    }
+  }
+  res.status(404).send('Short URL not found')
 })
 
 app.listen(port,(req,res)=>{
---
+++ shortUrl/randomURL.js
@@ -0,0 +1,14 @@
+let characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// 這個函式用來隨機生成網址，每次都從characters調用一個新的隨機字元加到shortUrl
+function gertshortUrl() {
+  let shortUrl = '';
+    for (let i = 0; i < 5; i++) {
+      let IndexRandom = Math.floor(Math.random() * characters.length);
+      shortUrl += characters[IndexRandom];
+      characters.splice(IndexRandom, 1);
+    }
+  return shortUrl
+}
+
+module.exports = gertshortUrl  //輸出檔案以供調用
