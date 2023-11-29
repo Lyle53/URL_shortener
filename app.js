c4d791fb8df58433561f1e3fea122227409c0b04
Lyle53 <lyle9453@gmail.com>
2023-11-29 15:21:24 +0800

    Controller:set router

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+// set router
 app.get('/', (req, res) =>{
-  res.send('express app')
+  res.redirect('/URL_Shortener')
+})
+app.get('/URL_Shortener',(req, res) =>{
+  res.render('index')
 })
 
-app.listen(port,()=>{
+app.listen(port,(req,res)=>{
   console.log(`express sever is running on http://localhost:${port}`)
 })
